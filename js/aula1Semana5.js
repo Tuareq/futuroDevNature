edeb25b455a263db075dbd3985c3c9dff8857456
Tuareq <nbn10developer@gmail.com>
2024-02-19 20:26:25 -0300

    Commit aula 1 semana 5

---
+++ js/aula1Semana5.js
@@ -0,0 +1,22 @@
+//variaveis simnples
+//let numero =15;
+
+let arrayNomes = ["Bruno", "Josue", "Narciso"]   // :10 indica a quantidade maxima de itens na sua lista pode ser qualquer uma
+// nos arrays tem indices e sempre começa por 0, 1, 2, 3.....
+arrayNomes.push('Maria')
+console.log(arrayNomes)
+
+
+//let nota1 = 8
+//let nota2 = 9
+//let nota3 = 7
+//let nota4 = 7
+
+// Pode usar o LET ou CONST o simbolo é colchet (lista)
+
+//let arrayNomes = [5, 5, 7, 7]
+
+//let soma = nota1 + nota2 + nota3 + nota4 
+
+//let media = soma / 4
+//let (media >= 7)  {console.log("ALUNO APROVADO")}
