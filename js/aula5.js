02e48c3f71f3c6588785a3bcabd1dd709b42e8dd
Tuareq <nbn10developer@gmail.com>
2024-02-19 20:26:22 -0300

    Commit aula 1 semana 5

---
+++ js/aula5.js
@@ -0,0 +1,38 @@
+//variaveis simnples
+//let numero =15;
+
+let arrayNomes = ["Bruno", "Josue", "Narciso", "Maria"]   // :10 indica a quantidade maxima de itens na sua lista pode ser qualquer uma
+// nos arrays tem indices e sempre começa por 0, 1, 2, 3.....
+//arrayNomes.push('Maria')
+//console.log(arrayNomes)
+
+console.log(arrayNomes.join(""))
+
+
+
+//let nota1 = 8
+//let nota2 = 9
+//let nota3 = 7
+//let nota4 = 7
+
+// Pode usar o LET ou CONST o simbolo é colchet (lista)
+
+let notas = [5, 5, 7, 7]
+let media = 0
+let soma = 0
+
+ for(let contador =0; contador < notas.length;contador++){
+    soma = soma + notas[contador]
+ }
+
+//let soma = nota1 + nota2 + nota3 + nota4 
+
+ media = soma /  notas.length;
+
+ if (media >= 7)  {
+   console.log("ALUNO APROVADO") 
+} else {
+       console.log("ALUNO REPROVADO")}
+
+// Para executar o script CTRL + ALT + N   
+// Para tirar ou colocar o sinal de // clicar CTRL + ; ou CTRL + SHIFT + :
