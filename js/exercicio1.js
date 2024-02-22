2e8884a371f4ca0ad09edd8ea7090293faa9f8d0
Tuareq <nbn10developer@gmail.com>
2024-02-22 17:46:32 -0300

    exercico1

---
+++ js/exercicio1.js
@@ -0,0 +1,8 @@
+// EXERCICIO 1 M1S05
+const prompt = require('prompt-sync')({sigint: true});
+ let frutas = [];
+ for (let i = 0; i < 3; i++) {
+    let fruta = prompt(`Digite a fruta ${i+1}: `);
+     frutas.push(fruta);
+ }
+ console.log(`A segunda fruta é ${frutas[1]}`);
