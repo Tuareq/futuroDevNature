9b2ca03c33d1213f76a11fb4bb80bfe0d3fb6937
Tuareq <nbn10developer@gmail.com>
2024-02-23 20:53:49 -0300

    atual

--- js/exercicio7.js
+++ js/exercicio7.js
@@ -1,5 +1,8 @@
 const numeros = [1, 2, 3, 4, 5];
 
-map() const quadrados = numeros.map(numero => numero ** 2); 
+numeros.map(numero => {
+    const quadrados = numero ** 2;
+    return quadrados;
+  });
 
 console.log(quadrados); // [1, 4, 9, 16, 25]
