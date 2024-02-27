f29c1fd01b32baef8f7bbd0d17e49f9833cca497
Tuareq <nbn10developer@gmail.com>
2024-02-27 19:45:05 -0300

    m1s06ex2

--- js/m1s06ex2.js
+++ js/m1s06ex2.js
@@ -1,14 +1,29 @@
+async function getUserInfo() {
+  return new Promise((resolve) => {
+    // Simulando o tempo de espera da consulta (2 segundos)
+    setTimeout(() => {
+      const userInfo = {
+        nome: "Alice",
+        idade: 30,
+        email: "alice@example.com",
+      };
+      resolve(userInfo);
+    }, 2000);
+  });
+}
+
+// Função que utiliza getUserInfo para obter as informações do usuário e exibi-las no console
 async function obterEExibirInformacoesDoUsuario() {
-    try {
-      const userInfo = await getUserInfo();
-      console.log("Informações do usuário:");
-      console.log(`Nome: ${userInfo.nome}`);
-      console.log(`Idade: ${userInfo.idade}`);
-      console.log(`Email: ${userInfo.email}`);
-    } catch (error) {
-      console.error("Erro ao obter informações do usuário:", error.message);
-    }
+  try {
+    const userInfo = await getUserInfo();
+    console.log("Informações do usuário:");
+    console.log(`Nome: ${userInfo.nome}`);
+    console.log(`Idade: ${userInfo.idade}`);
+    console.log(`Email: ${userInfo.email}`);
+  } catch (error) {
+    console.error("Erro ao obter informações do usuário:", error.message);
   }
-  
-  // Chamando a função para obter e exibir as informações do usuário
-  obterEExibirInformacoesDoUsuario();
+}
+
+// Chamando a função para obter e exibir as informações do usuário
+obterEExibirInformacoesDoUsuario();
