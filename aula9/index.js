b22f3d16c2216e5f0aa3cabc6bd872116567265d
Tuareq <nbn10developer@gmail.com>
2024-03-25 21:05:03 -0300

    aula9

--- aula9/index.js
+++ aula9/index.js
@@ -2,4 +2,7 @@ const express = require("express")
 
 const app = express();
 
-app.listen(3000);   // padrao é a porta 3000
+app.listen(3000, funcition(){
+    console.log("Minha aplicação esta no ar")
+}); 
+// padrao é a porta 3000
