10ae07df43adbf9ea8d9037a0eaff900eb8a88fe
Tuareq <nbn10developer@gmail.com>
2024-03-27 16:51:18 -0300

    aula9

--- aula9/index.js
+++ aula9/index.js
@@ -1,26 +1,37 @@
-const express = require("express")
-
+const express = require("express");
 const app = express();
+
+const requestLogger = (req, res, next) => {
+  const { method, url } = req;
+  const timestamp = new Date().toISOString();
+  console.log(`[${timestamp}] ${method} ${url}`);
+  next();
+};
+
+app.use(requestLogger);
 
 app.get("/", function(req, res){
     res.send("Olá Mundo")
-})  //adicionar rota dizendo o metodo
+});
 
 app.get("/sobre", function(req, res){
     res.send("Olá sobre ta certo")
-}) 
+});
+
 app.get("/contato", function(req, res){
     res.send("Quem é este contato?")
-}) 
+});
+
 app.get("/sobre:nome", function(req, res){
     res.send("O seu nome é:")
-}) 
-app.get("/ola/:nome", function(req, res) {
-    const nome = req.params.nome
+});
 
-    res.send("Bem vindo " + nome)
-})
-// Servidor padrao é a porta 3000
+app.get("/ola/:nome", function(req, res) {
+    const nome = req.params.nome;
+
+    res.send(`Bem vindo ${nome}`);
+});
+
 app.listen(3000, function(){
-    console.log("Minha aplicação esta no ar!")
-}); 
+    console.log("Minha aplicação esta no ar!");
+});
