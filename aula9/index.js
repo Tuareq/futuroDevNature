3e3e78a65414d2f104e8a1115c3be1c2b26b1287
Tuareq <nbn10developer@gmail.com>
2024-03-27 16:43:08 -0300

    aula9

--- aula9/index.js
+++ aula9/index.js
@@ -17,7 +17,7 @@ app.get("/ola/:nome", function(req, res) {
 
     res.send("Bem vindo " + nome)
 })
-// Servidro padrao é a porta 3000
+// Servidor padrao é a porta 3000
 app.listen(3000, function(){
     console.log("Minha aplicação esta no ar!")
 }); 
